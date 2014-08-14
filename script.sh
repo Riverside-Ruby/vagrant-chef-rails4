08aee29dc1738e6534a096117cc1a99754fb7ba6
Jack Lucky <jack.lucky.iv@gmail.com>
2014-08-14 10:44:36 -0700

    Make Rails app bundle, migrate, and run server

--- script.sh
+++ script.sh
@@ -12,4 +12,5 @@
 RAILS_DIR='/vagrant/rails'
 su - vagrant -c "git clone https://github.com/Riverside-Ruby/topics-project ${RAILS_DIR}"
 su - vagrant -c "cd ${RAILS_DIR} && bundle"
-su - vagrant -c "cd ${RAILS_DIR} && rake db:create db:migrate db:seed"
+su - vagrant -c "rake -f ${RAILS_DIR}/Rakefile db:create db:migrate db:seed"
+su - vagrant -c "cd ${RAILS_DIR} && rails s -d"
